a16efc90d69e4d03570a6cd82907f08e99f9eaab
Wasiq <wasiqbarat@gmail.com>
2024-11-04 05:47:11 +0330

    Docker Compose Created

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def server_error(error):
     return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
